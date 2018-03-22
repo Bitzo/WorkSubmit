03d9688d585686bb5e1164fc6d0996c2fe9c42c6
bitzo <bitzo@qq.com>
2018-03-22 10:47:00 +0800

    update: 完成界面

---
+++ public/javascripts/angular/baseController.js
@@ -0,0 +1,10 @@
+/**
+ * @Author: bitzo
+ * @Date: 17-10-4 下午3:47
+ * @Last Modified by: bitzo
+ * @Last Modified time: 17-10-4 下午3:47
+ * @Function:
+ */
+myApp.controller('baseController', function($scope, $http) {
+
+});
---
+++ public/javascripts/angular/baseconfig.js
@@ -0,0 +1,12 @@
+/**
+ * @Author: bitzo
+ * @Date: 17-10-4 下午3:15
+ * @Last Modified by: bitzo
+ * @Last Modified time: 17-10-4 下午3:15
+ * @Function:
+ */
+var myApp = angular.module('myApp', []);
+
+myApp.config(function() {
+
+});
